dc900d604b1b44572ead92db6d3fe3895f06a1db
Luis Lucas <lucas.uni@gmail.com>
2019-03-30 09:23:42 +0100

    Initial Commit

---
+++ Ej1MCSendString/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ej1MCSendString
+//
+//  Created by lucas on 30/03/2019.
+//  Copyright © 2019 lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

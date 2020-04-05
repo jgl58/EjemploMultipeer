312464603fd2b607adbbf0dfc1e7f4d9752fe934
Jonay Gilabert López <jgl58@alu.ua.es>
2020-04-05 18:59:53 +0200

    Conexion peers

---
+++ Ej1MCSendString/AppAlert.swift
@@ -0,0 +1,77 @@
+//
+//  AppAlert.swift
+//  Ej1MCSendString
+//
+//  Created by Jonay Gilabert López on 05/04/2020.
+//  Copyright © 2020 lucas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class AppAlert {
+    private var alertController: UIAlertController
+
+    public init(title: String? = nil, message: String? = nil, preferredStyle: UIAlertController.Style) {
+        self.alertController = UIAlertController(title: title, message: message, preferredStyle: preferredStyle)
+    }
+
+    public func setTitle(_ title: String) -> Self {
+        alertController.title = title
+        return self
+    }
+
+    public func setMessage(_ message: String) -> Self {
+        alertController.message = message
+        return self
+    }
+
+    public func setPopoverPresentationProperties(sourceView: UIView? = nil, sourceRect:CGRect? = nil, barButtonItem: UIBarButtonItem? = nil, permittedArrowDirections: UIPopoverArrowDirection? = nil) -> Self {
+
+        if let poc = alertController.popoverPresentationController {
+            if let view = sourceView {
+                poc.sourceView = view
+            }
+            if let rect = sourceRect {
+                poc.sourceRect = rect
+            }
+            if let item = barButtonItem {
+                poc.barButtonItem = item
+            }
+            if let directions = permittedArrowDirections {
+                poc.permittedArrowDirections = directions
+            }
+        }
+
+        return self
+    }
+
+    public func addAction(title: String = "", style: UIAlertAction.Style = .default, handler: @escaping ((UIAlertAction?) -> Void) = { _ in }) -> Self {
+        alertController.addAction(UIAlertAction(title: title, style: style, handler: handler))
+        return self
+    }
+
+    public func addTextFieldHandler(_ handler: @escaping ((UITextField?) -> Void) = { _ in }) -> Self {
+        alertController.addTextField(configurationHandler: handler)
+        return self
+    }
+
+    public func build() -> UIAlertController {
+        return alertController
+    }
+}
+
+
+public extension UIAlertController {
+    func showAlert(animated: Bool = true, completionHandler: (() -> Void)? = nil) {
+        guard let rootVC = UIApplication.shared.keyWindow?.rootViewController else {
+            return
+        }
+        var forefrontVC = rootVC
+        while let presentedVC = forefrontVC.presentedViewController {
+            forefrontVC = presentedVC
+        }
+        forefrontVC.present(self, animated: animated, completion: completionHandler)
+    }
+}
+
--- Ej1MCSendString/ViewController.swift
+++ Ej1MCSendString/ViewController.swift
@@ -7,14 +7,72 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
+
+    @IBOutlet weak var textToSend: UITextField!
+    @IBOutlet weak var connectionsLabel: UILabel!
+    @IBOutlet weak var textRecievedLabel: UILabel!
+    
+    @IBOutlet weak var listaPeers: UITableView!
+    let sendTextService = SendTextService()
+    
+    var listaUsuarios : [String] = []
+
+    @IBOutlet weak var list: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.listaUsuarios = [String]()
+        sendTextService.delegate=self
+        self.list.text = ""
+
     }
 
-
+    @IBAction func sendText(_ sender: Any) {
+        let text = self.textToSend.text ?? "Texto vacío"
+        sendTextService.send(text: text)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.listaUsuarios.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MiCelda", for: indexPath)
+        cell.detailTextLabel?.text = self.listaUsuarios[indexPath.row]
+        return cell
+    }
+    
 }
+
+
+extension ViewController: SendTextServiceDelegate {
+    
+    func connectedDevicesChanged(manager: SendTextService, connectedDevices: [String]) {
+        OperationQueue.main.addOperation {
+            self.connectionsLabel.text = "\(connectedDevices)"
+        }
+    }
+    
+    func sendTextService(didReceive text: String) {
+        
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "es-ES")
+        
+        let synthesizer = AVSpeechSynthesizer()
+        
+        OperationQueue.main.addOperation {
+            synthesizer.speak(utterance)
+            self.textRecievedLabel.text = text
+        }
+    }
+    
+}
+
+
 
